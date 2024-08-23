18268d7ff26c9d4134e56e92120b6e7f813dda5a
Aaron Bengochea <71156554+aaronbengochea@users.noreply.github.com>
2024-08-22 23:57:17 -0400

    adding backend comments

--- back/server.js
+++ back/server.js
@@ -8,12 +8,16 @@ const port = 4000
 app.use(express.json())
 app.use(cors())
 
+//add DB creds + .env
+//create basic api functions tomm
+
 app.get('/visit', (req, res) => {
     res.json({ message: 'Hello from the backend!' });
     console.log("ping")
   });
-  
-  
+
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
